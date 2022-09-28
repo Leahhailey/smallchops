d6ae3c2bbf0257bfd421af32cb3156c12b549be0
rosh <roshbon@gmail.com>
2022-09-28 15:45:01 +0100

    added mysql and implemented the signup feature

--- index.js
+++ index.js
@@ -1,43 +1,80 @@
+require('dotenv').config()
 const express = require('express')
+const mysql   = require('mysql')
 const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid');
 const app = express()
 const port = 8000
 
 app.use(bodyParser.json())
-
-
-const users = [
-    {
-        id: 1,
-        firstname: "Qudus",
-        othername: "Adedokun",
-        occupation: "Jobless",
-        age: 18
-    },
-    
-    {
-        id: 2,
-        firstname: "Qudus",
-        othername: "Zubair",
-        occupation: "Retired",
-        age: 22
-    },
-    {
-        id: 3,
-        firstname: "Adedeji",
-        othername: "Aminah",
-        occupation: "Doctor",
-        age: 5
-    }
-]
-
-
 
 app.listen(port, () => {
 
     console.log(`smallchops api is listening on port ${port}`)
     
 })
+
+const connection = mysql.createConnection({
+    host     : process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME
+  });
+
+connection.connect()
+
+
+//create user
+app.post('/create', (req, res) => { 
+
+    const { firstname, othername, phone, email } = req.body
+
+    // 1. validate the inputs
+    //2. comfirm the email or phone does not exist
+    //3. if 2 is true, return email or phone exist
+    //4. else create teh customer 
+
+    if (!firstname || !othername || !phone || !email) { 
+
+        return res.status(400)
+            .json(
+                { message: 'Please fill all fields' } )
+    } 
+
+    connection.query(`SELECT * from customers where email='${email}' or phone='${phone}'`,
+        (error, results, fields) => {
+            if (error) {
+                console.log("here1: " , error)
+                res.status(500).json( { message: 'An error occured' } )
+            }
+            if(results.length > 0) {
+                res.status(400).json( { message: 'Email or phone already exists' } )
+            }
+
+            let customer_id = uuidv4()
+            
+            connection.query(`INSERT INTO customers (customer_id, firstname, othernames, phone, email) 
+                              VALUES ('${customer_id}','${firstname}', '${othername}', '${phone}', '${email}')`),
+                (error, results, fields) => { 
+
+                    if (error) {
+                        console.log("here2: " , error)
+                         res.status(500).json( { message: 'An error occured' } )
+                    }
+                     res.status(201).json( { message: 'Customer created' } )
+                }
+        
+
+            
+      });
+
+
+      
+
+
+})
+
 
 
 
@@ -62,7 +99,6 @@ app.get('/user/:_id', (req, res) => {
 
 })
 
-
 //get all users
 app.get('/users', (req, res) => { 
 
@@ -74,26 +110,3 @@ app.get('/users', (req, res) => {
 })
 
 
-//create user
-
-app.post('/create', (req, res) => { 
-
-    const { firstname, othername, occupation, age } = req.body
-
-    const user = {
-        id: users.length + 1,
-        firstname: firstname,   
-        othername: othername,
-        occupation: occupation,
-        age: age
-    }
-
-    users.push(user)
-
-    res.status(200).send({
-        message: "user created",
-        data: user
-    })
-
-
-})
