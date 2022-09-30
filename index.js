f1f4c8c58b1d52d7c7b8778d2d97634620b3ff9f
rosh <roshbon@gmail.com>
2022-09-30 12:09:55 +0100

    refractorred

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
 const express = require('express')
-const mysql   = require('mysql')
 const bodyParser = require('body-parser')
-const { v4: uuidv4 } = require('uuid');
+const mysql = require('mysql2')
+const { v4: uuidv4 } = require('uuid')
 const app = express()
-const port = 8000
+const port = 5500
 
 app.use(bodyParser.json())
 
@@ -14,63 +13,94 @@ app.listen(port, () => {
     
 })
 
+// create the connection to database
 const connection = mysql.createConnection({
-    host     : process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    port: process.env.DATABASE_PORT,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
-  });
-
-connection.connect()
+    host: 'localhost',
+    user: 'root',
+    port:'8889',
+    password: 'root',
+    database: 'smallchops'
+});
+  
 
 
 //create user
 app.post('/create', (req, res) => { 
 
-    const { firstname, othername, phone, email } = req.body
 
-    // 1. validate the inputs
-    //2. comfirm the email or phone does not exist
-    //3. if 2 is true, return email or phone exist
-    //4. else create teh customer 
+    const { firstname, othername, email, phone } = req.body
+    
+    //1. validate teh varibles
+    //2. check if email or phone exist
+    //3. if email exist , return a message that email or phonne already exist
+    //4. else create the customer
 
-    if (!firstname || !othername || !phone || !email) { 
+    if (!firstname || !othername || !email || !phone ) {
+        res.status(400).json({
+            message: "All fields are required"
+        })
+    }
 
-        return res.status(400)
-            .json(
-                { message: 'Please fill all fields' } )
-    } 
-
-    connection.query(`SELECT * from customers where email='${email}' or phone='${phone}'`,
-        (error, results, fields) => {
-            if (error) {
-                console.log("here1: " , error)
-                res.status(500).json( { message: 'An error occured' } )
+    connection.query(
+        `select * from customers where email='${email}' or phone='${phone}'`,
+        (err, results, fields)  => {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+            if (err) {
+                console.log(err.sqlMessage)
+                res.status(400).json({
+                    message: "An error cocured "
+                })
             }
-            if(results.length > 0) {
-                res.status(400).json( { message: 'Email or phone already exists' } )
+
+           
+            if (results.length > 0) {
+                res.status(400).json({ 
+                    message: "This email/phone exists"
+                })
             }
 
-            let customer_id = uuidv4()
-            
-            connection.query(`INSERT INTO customers (customer_id, firstname, othernames, phone, email) 
-                              VALUES ('${customer_id}','${firstname}', '${othername}', '${phone}', '${email}')`),
-                (error, results, fields) => { 
 
-                    if (error) {
-                        console.log("here2: " , error)
-                         res.status(500).json( { message: 'An error occured' } )
-                    }
-                     res.status(201).json( { message: 'Customer created' } )
+            //create the customer 
+            let customer_id =  uuidv4()
+            connection.query(
+                `insert into customers(customer_id,firstname, othernames, phone, email)
+                 values('${customer_id}','${firstname}','${othername}','${phone}', '${email}' )`,
+                 (err, results, fields)  => {
+                     if (err) {
+                         console.log("error: ", err.sqlMessage)
+                         res.status(400).json({
+                         message: "An error occured"
+                     })
+                     }
+
+
+                     res.status(201).json({
+                         message: "Account succesfully created",
+                         data: {
+                             firstname,
+                             othername,
+                             phone,
+                             email
+                         }
+                     })
+                     
+
+                    
+
                 }
-        
+              );
+              
 
-            
-      });
+        }
+      );
 
 
-      
+
+
+
+
+
 
 
 })
