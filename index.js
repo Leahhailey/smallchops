44649b55a830f7c62fd66c49327737e6d034ba13
rosh <roshbon@gmail.com>
2022-09-30 15:59:28 +0100

    added joi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
 const { v4: uuidv4 } = require('uuid')
+const Joi = require('joi')
 const app = express()
 const port = process.env.APP_PORT
 
@@ -14,7 +15,7 @@ app.listen(port, () => {
     
 })
 
-// create the connection to database
+//create the connection to database
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -22,80 +23,93 @@ const connection = mysql.createConnection({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME
 });
-  
 
 
 //create user
 app.post('/create', (req, res) => { 
 
 
-    const { firstname, othername, email, phone } = req.body
+    const schema = Joi.object({
+        firstname: Joi.string().min(4).max(30).required(),
+        othername: Joi.string().min(4).max(30).required(),
+        phone: Joi.string().min(11).max(14).required(),
+        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    })
     
+    const { error, value } = schema.validate(req.body);
+    
+    if (error != "undefined") {
+        
+        res.status(400).send({
+            message: 'validation error'
+        })
+    }
+
+    const { firstname, othername, email, phone } = req.body
+
     //1. validate teh varibles
     //2. check if email or phone exist
     //3. if email exist , return a message that email or phonne already exist
     //4. else create the customer
+    try {
+        if (!firstname || !othername || !email || !phone) {
+            throw new Error ('All fields are required')
+       
+        }
 
-    if (!firstname || !othername || !email || !phone ) {
-        res.status(400).json({
-            message: "All fields are required"
-        })
-    }
-
-    connection.query(
-        `select * from customers where email='${email}' or phone='${phone}'`,
-        (err, results, fields)  => {
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-            if (err) {
-                console.log(err.sqlMessage)
-                res.status(400).json({
-                    message: "An error cocured "
-                })
-            }
+        connection.query(
+            `select * from customers where email='${email}' or phone='${phone}'`,
+            (err, results, fields) => {
+                 if (err) {
+                   console.log('errr: ', err)
+                    throw new Error ('Please check back, this is on us.')
+                }
 
            
-            if (results.length > 0) {
-                res.status(400).json({ 
-                    message: "This email/phone exists"
-                })
-            }
+                if (results.length > 0) {
+                    throw new Error ('The email/Phone exists.', 400)
+                }
 
 
-            //create the customer 
-            let customer_id =  uuidv4()
-            connection.query(
-                `insert into customers(customer_id,firstname, othernames, phone, email)
+                //create the customer 
+                let customer_id = uuidv4()
+                connection.query(
+                    `insert into customers(customer_id,firstname, othernames, phone, email)
                  values('${customer_id}','${firstname}','${othername}','${phone}', '${email}' )`,
-                 (err, results, fields)  => {
-                     if (err) {
-                         console.log("error: ", err.sqlMessage)
-                         res.status(400).json({
-                         message: "An error occured"
-                     })
-                     }
+                    (err, results, fields) => {
+                        if (err) {
+                            // console.log("error: ", err.sqlMessage)
+                            throw new Error("This is on us, pleae try later")
+                        }
 
 
-                     res.status(201).json({
-                         message: "Account succesfully created",
-                         data: {
-                             firstname,
-                             othername,
-                             phone,
-                             email
-                         }
-                     })
+                        res.status(201).json({
+                            message: "Account succesfully created",
+                            data: {
+                                firstname,
+                                othername,
+                                phone,
+                                email
+                            }
+                        })
                      
 
                     
 
-                }
-              );
+                    }
+                );
               
 
-        }
-      );
+            }
+        );
+       
+    } catch (e) {
 
+        res.status(400).json({
+            message: e.message
+        })
+
+    }
 
 
 
@@ -141,3 +155,5 @@ app.get('/users', (req, res) => {
 })
 
 
+
+// connection.end()
